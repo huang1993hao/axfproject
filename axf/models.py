0f743823439ae7d94f76e916a5959a5156244244
huang1993hao <964353126@qq.com>
2018-11-02 01:50:00 -0700

    首页完成

--- axf/models.py
+++ axf/models.py
@@ -24,5 +24,36 @@ class Nav(Base):
 class Mustbuy(Base):
     class Meta:
         db_table = 'axf_mustbuy'
+#商品部分
+class Shop(Base):
+    class Meta:
+        db_table = 'axf_shop'
+# 商品主体内容
+class MainShow(models.Model):
+    trackid = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=100)
+    categoryid = models.CharField(max_length=8)
+    brandname = models.CharField(max_length=50)
+    img1 = models.CharField(max_length=100)
+    childcid1 = models.CharField(max_length=8)
+    productid1 = models.CharField(max_length=8)
+    longname1 = models.CharField(max_length=100)
+    price1 = models.FloatField()
+    marketprice1 = models.FloatField()
+    img2 = models.CharField(max_length=100)
+    childcid2 = models.CharField(max_length=8)
+    productid2 = models.CharField(max_length=8)
+    longname2 = models.CharField(max_length=100)
+    price2 = models.FloatField()
+    marketprice2 = models.FloatField()
+    img3 = models.CharField(max_length=100)
+    childcid3 = models.CharField(max_length=8)
+    productid3 = models.CharField(max_length=8)
+    longname3 = models.CharField(max_length=100)
+    price3 = models.FloatField()
+    marketprice3 = models.FloatField()
+    class Meta:
+        db_table = 'axf_mainshow'
 
 
--- axf/views.py
+++ axf/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from axf.models import Wheel, Nav, Mustbuy
+from axf.models import Wheel, Nav, Mustbuy, Shop, MainShow
 
 
 def home(request):# 首页
@@ -12,10 +12,22 @@ def home(request):# 首页
     navs = Nav.objects.all()
     # 每日必购
     mustbuys = Mustbuy.objects.all()
+    shopList = Shop.objects.all()
+    shophead = shopList[0]
+    shoptab = shopList[1:3]
+    shopclass = shopList[3:7]
+    shopcommend = shopList[7:11]
+    # 商品主体内容
+    mainshows = MainShow.objects.all()
     data = {
         'wheels': wheels,
         'navs': navs,
         'mustbuys': mustbuys,
+        'shophead': shophead,
+        'shoptab': shoptab,
+        'shopclass': shopclass,
+        'shopcommend': shopcommend,
+        'mainshows': mainshows
     }
     return render(request,'home/home.html',context=data)
 
