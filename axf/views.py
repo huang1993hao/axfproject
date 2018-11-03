edd55400ea1f69caaac41eb820b96e00b40dd6d8
huang1993hao <964353126@qq.com>
2018-11-02 19:53:46 -0700

    超市功能完成

--- axf/views.py
+++ axf/views.py
@@ -32,15 +32,45 @@ def home(request):# 首页
     return render(request,'home/home.html',context=data)
 
 
-def market(request,categoryid):
+def market(request,categoryid,childid,sortid):
     # 分类信息
     foodtypes = Foodtypes.objects.all()
+    # 分类 点击 下标  >>>>  分类ID
+    typeIndex = int(request.COOKIES.get('typeIndex', 0))
+    # 根据分类下标 获取 对应 分类ID
+    categoryid = foodtypes[typeIndex].typeid
+    # 子类信息
+    childtypenames = foodtypes.get(typeid=categoryid).childtypenames
+    # 将每个子类拆分出来
+    childTypleList = []
+    for item in childtypenames.split('#'):
+        arr = item.split(':')
+        dir = {
+            'childname':arr[0],# 子类名称
+            'childid':arr[1]  # 子类ID
+        }
+        childTypleList.append(dir)
     # 商品信息 - 根据分类id获取对应数据
     # goodsList = Goods.objects.all()[0:5]
-    goodsList = Goods.objects.filter(categoryid=categoryid)
+    if childid == '0':  # 全部分类
+        goodsList = Goods.objects.filter(categoryid=categoryid)
+    else:# 分类 下 子类
+        goodsList = Goods.objects.filter(categoryid=categoryid,childcid=childid)
+
+    # 排序
+    if sortid == '1':# 销量排序
+        goodsList = goodsList.order_by('-productnum')
+    elif sortid == '2':  # 价格最低
+        goodsList = goodsList.order_by('price')
+    elif sortid == '3':  # 价格最高
+        goodsList = goodsList.order_by(('-price'))
+
     data = {
-        'foodtypes': foodtypes,
-        'goodsList': goodsList,
+        'foodtypes': foodtypes, # 分类信息
+        'goodsList': goodsList,# 商品信息
+        'childTypleList': childTypleList,  # 子类信息
+        'categoryid':categoryid, # 分类ID
+        'childid': childid,  # 子类ID
     }
     return render(request,'market/market.html',context=data)
 
--- static/market/js/market.js
+++ static/market/js/market.js
@@ -1,7 +1,13 @@
 $(function(){
     $('.market').width(innerWidth)
     // 获取typeIndex
-
+    typeIndex = $.cookie('typeIndex')
+    if (typeIndex){ // 已经有点击分类
+        $('.type-slider .type-item').eq(typeIndex).addClass('active')
+    } else {    // 没有点击分类
+        // 没有点击默认第一个
+        $('.type-slider .type-item:first').addClass('active')
+    }
      // 侧边栏
     // 问题: 点击时，样式已经添加。
     //       但这是a标签，需要重新获取页面，即重新刷新页面，样式就恢复到原来的
@@ -19,4 +25,52 @@ $(function(){
         // 记录位置
         $.cookie('typeIndex', $(this).index(), {expires:3, path:'/'})
     })
+     // 分类按钮
+    categoryBt = false  // 默认是隐藏
+    $('#categoryBt').click(function () {
+        // 取反
+        categoryBt = !categoryBt
+         categoryBt ? categoryViewShow() : categoryViewHide()
+         // if (categoryBt){    // 显示
+        //     $('.bounce-view.category-view').show()
+        //     $('#categoryBt i').removeClass('glyphicon-triangle-top').addClass('glyphicon-triangle-bottom')
+        // } else {    // 隐藏
+        //     $('.bounce-view.category-view').hide()
+        //     $('#categoryBt i').removeClass('glyphicon-triangle-bottom').addClass('glyphicon-triangle-top')
+        // }
+    })
+     // 排序按钮
+    sortBt = false  // 默认是隐藏
+    $('#sortBt').click(function () {
+        // 取反
+        sortBt = !sortBt
+         sortBt ? sortViewShow() : sortViewHide()
+    })
+    // 灰色蒙层
+    $('.bounce-view').click(function () {
+        sortBt = false
+        sortViewHide()
+        categoryBt = false
+        categoryViewHide()
+    })
+    function categoryViewShow() {
+        sortBt = false
+        sortViewHide()
+        $('.bounce-view.category-view').show()
+        $('#categoryBt i').removeClass('glyphicon-triangle-top').addClass('glyphicon-triangle-bottom')
+    }
+    function categoryViewHide() {
+        $('.bounce-view.category-view').hide()
+        $('#categoryBt i').removeClass('glyphicon-triangle-bottom').addClass('glyphicon-triangle-top')
+    }
+     function sortViewShow() {
+        categoryBt = false
+        categoryViewHide()
+        $('.bounce-view.sort-view').show()
+        $('#sortBt i').removeClass('glyphicon-triangle-top').addClass('glyphicon-triangle-bottom')
+    }
+    function sortViewHide() {
+        $('.bounce-view.sort-view').hide()
+        $('#sortBt i').removeClass('glyphicon-triangle-bottom').addClass('glyphicon-triangle-top')
+    }
 })
